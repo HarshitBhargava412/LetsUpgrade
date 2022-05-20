5dca6499886b3e7bf548c628db09e31e2693abb7
Harshit Bhargava <86331803+HarshitBhargava412@users.noreply.github.com>
2022-05-20 19:44:32 +0530

    Add files via upload

---
+++ Assignment 9 (Day 19)/app.js
@@ -0,0 +1,47 @@
+// console.log("I am Log");
+// console.warn("I am Warning");
+// console.error("I am Error");
+
+// alert("Hello Harshit Bhargava");
+// prompt("Enter Your Name");
+// confirm("Are You Sure??");
+
+
+
+// DataTypes
+
+var firstName = "Harshit";
+var lastName = 'Bhargava';
+var age = 20;
+var percentage = 87.25;
+var isAdmin = false;
+var checkUndo;
+var checkNull = null;
+var marks = [12, 34, 56, 7, 89];
+var address = {};
+
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(age);
+// console.log(percentage);
+// console.log(isAdmin);
+// console.log(checkUndo);
+// console.log(checkNull);
+// console.log(marks);
+// console.log(address);
+
+
+// console.log(typeof firstName);
+// console.log(typeof lastName);
+// console.log(typeof age);
+// console.log(typeof percentage);
+// console.log(typeof isAdmin);
+// console.log(typeof checkUndo);
+// console.log(typeof checkNull);
+// console.log(typeof marks);
+// console.log(typeof address);
+
+
+const PI = 3.14;
+console.log(PI);
