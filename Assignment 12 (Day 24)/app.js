9bd59ebb4cf6751e4084da2e92d4d04d608dbf03
Harshit Bhargava <86331803+HarshitBhargava412@users.noreply.github.com>
2022-05-23 00:22:43 +0530

    Add files via upload

---
+++ Assignment 12 (Day 24)/app.js
@@ -0,0 +1,80 @@
+let Laptops = [
+    {
+        id: 001,
+        name: 'New Apple MacBook Air (13-inch, 1.1GHz Dual-core 10th-Generation Intel Core i3 Processor, 8GB RAM, 256GB Storage) - Space Grey',
+        specifications : {
+            0: 'Stunning 13.3-inch Retina display with True Tone technology',
+            1: 'Backlit Magic Keyboard and Touch ID',
+            2: 'Tenth-generation Intel Core i3 processor',
+            3: 'Fast SSD storage',
+            4: '8GB of memory',
+            5: 'Stereo speakers with wider stereo sound',
+            6: 'Operating-System: IOS',
+            7: 'CPU Manufacturer: Intel'
+        },
+        image: 'lappy1.png',
+        details: {
+            price: 92990,
+            Assured: 'Yes',
+            Discount: '9000 on IndusInd Bank'
+        },
+        inCart: 0
+    },
+    {
+        id: 002,
+        name: 'ASUS ZenBook Duo UX481FL-BM5811T Intel Core i5 10th Gen 14-inch FHD Thin and Light Laptop (8GB RAM/512GB NVMe SSD/Windows 10/2GB NVIDIA GeForce MX250 Graphics/ScreenPad Plus/1.50 Kg), Celestial Blue',
+        image: 'lappy2.png',
+        specifications: {
+            0: 'Processor: 10th Gen Intel Core i5-10210U Processor 1.6 GHz',
+            1: 'Memory: 8GB Onboard LPDDR3 2133MHz RAM | Storage: 512GB SSD',
+            2: 'Graphics: Dedicated NVIDIA GeForce MX250 GDDR5 2GB VRAM',
+            3: 'Display: 14” LED-backlit FHD (1920 x 1080) 16:9 Anti-Glare Display, 90% screen-to-body ratio, 178 Degree wide-view technology, 100% sRGB, PANTONE Validated',
+            4: 'Design & battery: Spun-Metal finish| 3.5mm-thin bezels | Thin and Light Laptop | Laptop weight 1.5 Kg | 70Whrs 4-cell lithium-polymer battery'
+        },
+        details: {
+            price: 89980,
+            Assured: 'No',
+            Discount: '900 on Exchange'
+        },
+        inCart: 0
+    },
+    {
+        id: 003,
+        name: 'HP 20-c417in AIO 2018 19.5-inch All-in-One Desktop (Celeron J4005/4GB/1TB/Windows 10/Integrated Graphics)',
+        image: 'lappy3.png',
+        specifications: {
+            0: '2 GhzGHz Intel Celeron J4005 processor',
+            1: '4GB DDR4 RAM',
+            2: '1TB 7200rpm hard drive',
+            3: '19.5-inch screen, Intel Integrated Graphics',
+            4: 'Windows 10, Home operating system',
+            5: 'The latest processors, plenty of storage and a range of IPS display sizes power your content and give the whole family an awesome view. Now everyone can get connected without slowing down.'
+        },
+        details: {
+            price: 28719,
+            Assured: 'No',
+            Discount: '2900 on Hdfc Credit Card'
+        },
+        inCart: 0
+    },
+    {
+        id: 004,
+        name: 'Lenovo Ideacentre A340 8th Gen Intel Core I3 21.5 inch All in One Desktop (4GB/ 1 TB HDD/DOS/Integrated Graphics/DVD-RW/Black)',
+        image: 'lappy4.png',
+        specifications: {
+            0: 'Processor: 8th Generation Core Intel I3-8145U processor 2.1 Ghz base speed, 3.9 Ghz max speed, 2 cores, 4 Mb Smart Cache',
+            1: 'Memory and Storage: 4GB DDR4 RAM | 1TB Hard Disk Drive 5400 RPM | Integrated Graphics Card',
+            2: 'Operating System: This is a DOS based desktop. Requires separate purchase and installation of operating system software (like Windows), not included in the box . Refer to Lenovo website for drivers. Supported Operating Systems: Windows 10',
+            3: 'Display: 21.5 inch Full HD (1920 X 1080) Full HD Display | Anti Glare Technology | Borderless Display | 2 Side Narrow Bezel',
+            4: 'Ports: 2XUSB 2.0, 2X USB 3.1, HDMI Out | DVD-RW'
+        },
+        details: {
+            price: 34988,
+            Assured: 'Yes',
+            Discount: '4000 on SBI Debit Card'
+        },
+        inCart: 0
+    }
+];
+
+console.log(Laptops);
